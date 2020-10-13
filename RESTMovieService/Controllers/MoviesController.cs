937be3e6202953934cc1c313dc9f88b2f4e392ba
RasmusRB <rasmusbergstroem87@gmail.com>
2020-10-13 21:45:27 +0200

    asd

--- RESTMovieService/Controllers/MoviesController.cs
+++ RESTMovieService/Controllers/MoviesController.cs
@@ -17,31 +17,31 @@ namespace RESTMovieService.Controllers
     {
         ManageMovies mm = new ManageMovies();
 
-        private static readonly List<Movies> movies = new List<Movies>()
-        {
-            new Movies(1, "The Fellowship of the Ring", "Peter Jackson", "Fantasy", 2001, 8.8),
-            new Movies(2, "The Two Towers", "Peter Jackson", "Fantasy", 2002, 8.7),
-            new Movies(3, "Return of the King", "Peter Jackson", "Fantasy", 2003, 8.9),
-            new Movies(4, "The Terminator", "James Cameron", "Science Fiction", 1984, 8.0),
-            new Movies(5, "Terminator 2: Judgment Day", "James Cameron", "Science Fiction", 1991, 8.5),
-            new Movies(6, "Terminator 3: Rise of the Machines", "Jonathan Mostow", "Science Fiction", 2003, 6.3)
-        };
+        //private static readonly List<Movies> movies = new List<Movies>()
+        //{
+        //    new Movies(1, "The Fellowship of the Ring", "Peter Jackson", "Fantasy", 2001, 8.8),
+        //    new Movies(2, "The Two Towers", "Peter Jackson", "Fantasy", 2002, 8.7),
+        //    new Movies(3, "Return of the King", "Peter Jackson", "Fantasy", 2003, 8.9),
+        //    new Movies(4, "The Terminator", "James Cameron", "Science Fiction", 1984, 8.0),
+        //    new Movies(5, "Terminator 2: Judgment Day", "James Cameron", "Science Fiction", 1991, 8.5),
+        //    new Movies(6, "Terminator 3: Rise of the Machines", "Jonathan Mostow", "Science Fiction", 2003, 6.3)
+        //};
 
         // GET from substring - fx. api/movies/Title/The Terminator 
-        [HttpGet]
-        [Route("Title/{substring}")]
-        public IEnumerable<Movies> GetFromSubstring(String substring)
-        {
-            return movies.FindAll(m => m.Title.Contains(substring));
-        }
+        //[HttpGet]
+        //[Route("Title/{substring}")]
+        //public IEnumerable<Movies> GetFromSubstring(String substring)
+        //{
+        //    return movies.FindAll(m => m.Title.Contains(substring));
+        //}
 
-        // GET from substring - fx. api/movies/Genre/Science Fiction - watch capital letters
-        [HttpGet]
-        [Route("Genre/{substring}")]
-        public IEnumerable<Movies> GetByGenre (String substring)
-        {
-            return movies.FindAll(m => m.Genre.Contains(substring));
-        }
+        //// GET from substring - fx. api/movies/Genre/Science Fiction - watch capital letters
+        //[HttpGet]
+        //[Route("Genre/{substring}")]
+        //public IEnumerable<Movies> GetByGenre (String substring)
+        //{
+        //    return movies.FindAll(m => m.Genre.Contains(substring));
+        //}
 
         // GET: api/<MoviesController>
         [HttpGet]
@@ -51,19 +51,19 @@ namespace RESTMovieService.Controllers
         }
 
         // GET api/<MoviesController>/5
-        [HttpGet]
-        [Route("{id}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult GetByIdStatusCodes(int id)
-        {
-            if (movies.Exists(m => m.Id == id))
-            {
-                return Ok(movies.Find(m => m.Id == id));
-            }
+        //[HttpGet]
+        //[Route("{id}")]
+        //[ProducesResponseType(StatusCodes.Status200OK)]
+        //[ProducesResponseType(StatusCodes.Status404NotFound)]
+        //public IActionResult GetByIdStatusCodes(int id)
+        //{
+        //    if (movies.Exists(m => m.Id == id))
+        //    {
+        //        return Ok(movies.Find(m => m.Id == id));
+        //    }
 
-            return NotFound($"Movie id : {id} not found");
-        }
+        //    return NotFound($"Movie id : {id} not found");
+        //}
 
         [HttpGet]
         [Route("{id}")]
