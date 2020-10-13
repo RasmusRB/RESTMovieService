75dd818bc4c96b344f50f41b5c66ad6655abaafc
RasmusRB <rasmusbergstroem87@gmail.com>
2020-10-13 17:38:54 +0200

    With Swagger

--- RESTMovieService/Startup.cs
+++ RESTMovieService/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
 
 namespace RESTMovieService
 {
@@ -34,6 +36,11 @@ namespace RESTMovieService
                         builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
                 options.AddPolicy("AllowAnyOriginGetPut", builder => builder.AllowAnyOrigin().WithMethods("GET", "PUT").AllowAnyHeader());
             });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Movies API", Version = "v1.0" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +50,10 @@ namespace RESTMovieService
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Movies API v1.0")
+            );
 
             app.UseRouting();
 
